99dba8e46b33f29d1524739280cca560609aaafa
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-02 09:15:23 +0530

    added string methods

---
+++ 02-Strings/StringMethods.py
@@ -0,0 +1,16 @@
+
+s = "hello python"
+
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+
+print(s.replace("python" , "'world"))
+
+print("hello world".title())
+
+print("hello world".find("world"))
+
+print("banana".count('a'))
+
+
