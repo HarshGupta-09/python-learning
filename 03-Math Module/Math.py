6282bc56f4888d8f32830c1fef7efd2b32029059
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-06 10:29:49 +0530

    Added math and conditional statements

---
+++ 03-Math Module/Math.py
@@ -0,0 +1,12 @@
+
+import math
+
+print(math.gcd(20,38))
+print(math.factorial(38))
+
+print(math.sqrt(16))
+
+print(abs(-3333))
+
+print(math.lcm(20,38))
+
---
+++ 04-if-else/conditions.py
@@ -0,0 +1,37 @@
+
+# simple if 
+
+age = 20
+
+# if age > 18: 
+#  print("eligible")
+#  print('in if only')
+#  print("still in if")
+
+
+
+# print("out side if")
+
+# else if 
+
+if age >= 18:
+    print("Elgible for vote ")
+else : 
+    print("not eligible for vote")
+
+
+print('outside of if-else block')
+
+
+marks = int(input("Enter your marks : "))
+
+if marks >= 90 :
+    print("Grade A")
+elif marks >= 75:
+ print("Grade B")
+elif marks >= 60 :
+   print("Grade C")
+elif marks >= 34 :
+   print("Grade C")
+else : 
+   print("Fail")
