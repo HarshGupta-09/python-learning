cb9b64b8dfea46deb052ecde6e80490c46f36cf1
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-07 08:44:21 +0530

    Added loops

---
+++ 05-Loops/for.py
@@ -0,0 +1,15 @@
+
+name = "Harsh Gupta"
+
+for ch in name :
+    print(ch)
+
+print("end")
+
+for i in range(11) :
+    print(i , end = " ")
+  
+print()
+for i in range (0 , 21 , 2) : 
+    print(i)
+
