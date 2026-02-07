23f8a6cb06db3c91dbd6365efc6d82ad5dce8615
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-07 11:58:25 +0530

    Added tuples

--- 07-Data Structure/Lists/list.py
+++ 07-Data Structure/Lists/list.py
@@ -12,7 +12,7 @@ names.remove("Jai")
 
 # Program to find the largest number in  a list 
 
-nums = [12,23,434,533,45,33,56,3]
+nums = [12,23,434,533,45,33,56,33,33,33]
 max = -1
 
 for i in nums :
@@ -21,3 +21,12 @@ for i in nums :
 
 print(max)
 
+
+uniques = []
+
+for i in nums : 
+    if  i not in uniques :
+        uniques.append(i)
+
+print(nums)
+print(uniques)
---
+++ 07-Data Structure/Tuples/tuples.py
@@ -0,0 +1,8 @@
+
+t = (10,20,30,40,50)
+
+print(t)
+print(t[3])
+
+# t[3] = 33  Error  , tuples are immutalbe means cant modify 
+
