9bc53a0071ce1a67c3e20d1c7497cff62594b725
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-07 11:07:07 +0530

    added lsit

---
+++ 07-Data Structure/list.py
@@ -0,0 +1,23 @@
+
+names = ["Harsh" , "Diwakar" , "Jai", "Pandey","Electrical"]
+
+print(names)
+print(names[3])
+
+names.append("sonu")
+names.insert(2,"Sanskar")
+print(names)
+
+names.remove("Jai")
+
+# Program to find the largest number in  a list 
+
+nums = [12,23,434,533,45,33,56,3]
+max = -1
+
+for i in nums :
+    if i > max :
+        max = i
+
+print(max)
+
