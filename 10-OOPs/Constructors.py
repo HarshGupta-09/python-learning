221a11805f3e09d0308896df2710ddb0a212fc83
Harsh Gupta <harshgupta82003@gmail.com>
2026-02-10 09:48:59 +0530

    Added classes and constructor

---
+++ 10-OOPs/Constructors.py
@@ -0,0 +1,18 @@
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        
+
+
+
+    def move (self):
+        print("Move")
+
+    def draw (self) :
+        print("draw")
+
+
+point = Point(12,32)
+print(point.x)
